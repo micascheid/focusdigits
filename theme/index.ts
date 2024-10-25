c72d75a5f289710a4c348411e5463f3793dd0932
Mica Linscheid <micascheid@gmail.com>
2024-10-25 17:30:57 -0600

    initial commit

---
+++ theme/index.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#2196f3',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+});
+
+export default theme;
